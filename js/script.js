115be4921429db53d698c098add1237bd1cdc4d9
adrien <adrien.gonzalez@laplateforme.io>
2020-04-28 18:30:14 +0200

    Décrémentation des sous disponibles lors d'un achat

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+// MIONTANT MONAIE
+let monnaie = 500
+
 // BONUS 1 (dommage par clique)
 let clickdamage = 1
 let bonus_clickdamage = 2
 let niv_bonus_clique = 1
 let prix_bonus_clique = 5
+let achat_bonus_click = 0
+
 // BONUS 2 (dommage par seconde)
 let damage_seconde = 0
 let bonus_damage_seconde = 2
@@ -16,27 +21,66 @@ let niv_luck = 1
 let prix_bonus_luck = 1000
 
 
+// ETAT DU BOUTON SI DISPO ACHAT
+etat_bonus_clique()
+etat_bonus_damage_seconde()
+etat_bonus_luck()
+
+function etat_bonus_clique(){
+	$(document).ready(function(){
+		if(monnaie - prix_bonus_clique < 0)
+		{
+			$("#bonus_clique").text("Pas disponible")
+		}
+	});
+}
+
+function etat_bonus_damage_seconde(){
+	$(document).ready(function(){
+		if(monnaie - prix_bonus_damage < 0)
+		{
+			$("#auto_damage").text("Pas disponible")
+		}
+	});
+}
+
+function etat_bonus_luck(){
+	$(document).ready(function(){
+		if(monnaie - prix_bonus_luck < 0)
+		{
+			$("#luck").text("Pas disponible")
+		}
+	});
+}
+
 //FONCTION RANDOM CHIFFRE ALEATOIRE
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
 }
 
+function montant_monnaie(){
+
+	$("#montant").text(monnaie)
+
+}
 
 
 function bonus_clique()
 {
+	monnaie=monnaie-prix_bonus_clique
 	clickdamage = clickdamage + bonus_clickdamage
 	niv_bonus_clique = niv_bonus_clique +1
 	prix_bonus_clique = prix_bonus_clique + prix_bonus_clique
 	$("#click_damage").text("Dégâts par clique : "+clickdamage)
 	$("#click_profil").text("Dégâts par clique : "+clickdamage)
 	$("#niv_bonus_clique").text("Niv."+niv_bonus_clique)
-	console.log(prix_bonus_clique)
 	$("#prix_bonus_clique").text(prix_bonus_clique)
+	montant_monnaie()
 }
 
 function auto_damage()
 {
+	monnaie=monnaie-prix_bonus_damage
 	damage_seconde = damage_seconde + bonus_damage_seconde
 	niv_auto_damage = niv_auto_damage + 1
 	prix_bonus_damage = prix_bonus_damage + prix_bonus_damage
@@ -44,11 +88,12 @@ function auto_damage()
 	$("#damage_profil").text("Dégâts par seconde : "+damage_seconde)
 	$("#niv_auto_damage").text("Niv. "+niv_auto_damage)
 	$("#prix_bonus_auto_damage").text(prix_bonus_damage)
-
+	montant_monnaie()
 }
 
 function bonus_luck()
 {
+	monnaie=monnaie-prix_bonus_luck
 	if(luck > 3)
 	{
 		luck = luck - (luck / 2)
@@ -58,8 +103,6 @@ function bonus_luck()
 		$("#chance_critique").text("Dégâts critiques : "+parseInt(chance_critique * 100)+"%")
 		$("#niv_luck").text("Niv. "+niv_luck+"")
 		$("#prix_bonus_luck").text(prix_bonus_luck)
-		
-		console.log(parseInt(chance_critique * 100))
 	}
 	else if(luck < 3 && luck != 1)
 	{	
@@ -75,39 +118,39 @@ function bonus_luck()
 	{
 			// INFLIGER COUP CRITIQUE = clickdamage *X (X à définir)
 	}
+	montant_monnaie()
 }
-
-function bonus_money()
-{
-
-
-}
-
-
 
 
 $(document).ready(function(){
 
+	
+	montant_monnaie()
 	// BONUS POUR DEGATS PAR CLIQUE
 	$("body").on("click","#bonus_clique",function(){
+		etat_bonus_clique()
+		if(monnaie-prix_bonus_clique > 0)
+		{
+			bonus_clique()
 
-		bonus_clique()
+		}	
 	});
 
 	// BONUS POUR DEGATS PAR SECONDES (IDLE)
 	$("body").on("click","#auto_damage",function(){
-
-		auto_damage()
+		etat_bonus_damage_seconde()
+		if(monnaie-prix_bonus_damage > 0)
+		{
+			auto_damage()
+		}
 	});
 	// BONUS DE CHANCE CRITIQUE
-	$("body").on("click","#luck",function(){
-		
-		bonus_luck()
-	});
-	// BONUS SOUS
-	$("body").on("click","#money",function(){
-		
-		bonus_money()
+	$("body").on("click","#luck",function(){		
+		etat_bonus_luck()
+		if(monnaie-prix_bonus_luck > 0)
+		{
+			bonus_luck()
+		}
 	});
 });
 
