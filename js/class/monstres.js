633caa63f5c5c339290073592a38df3394047329
Mathilde <mathilde.roussier@laplateforme.io>
2020-04-29 17:27:33 +0200

    MAj des attribut => Privé

--- js/class/monstres.js
+++ js/class/monstres.js
@@ -1,45 +1,50 @@
 class Monstre {
 
+    #life;
+    #newLife;
+    #mort;
+    #nb_mort;
+
     constructor(vie) {
-        this.life = vie;
-        this.newLife = this.life;
-        this.mort = 10;
-        this.nb_mort = 0;
+        this.#life = vie;
+        this.#newLife = this.#life;
+        this.#mort = 10;
+        this.#nb_mort = 0;
     }
 
     getLife() {
-        return this.life;
+        return this.#life;
     }
 
     attaque(degat) {
-        this.newLife = this.newLife - degat;
+        this.#newLife = this.#newLife - degat;
     }
 
     getNewLife() {
-        return this.newLife;
+        return this.#newLife;
     }
 
     MajNewLife() {
-        this.newLife = this.life;
-        return this.newLife;
+        this.#newLife = this.#life;
+        return this.#newLife;
     }
 
     MajLife(X) {
-        this.life = this.life * X;
+        this.#life = this.#life * X;
     }
 
     setDpsNewLife(life_actuel, degatdps) {
-        this.newLife = life_actuel * 1 - degatdps;
+        this.#newLife = life_actuel * 1 - degatdps;
     }
 
     getMort() {
-        return this.mort;
+        return this.#mort;
     }
 
     setnb_mort() {
-        this.nb_mort = this.nb_mort + 1;
+        this.#nb_mort = this.#nb_mort + 1;
     }
     getnb_mort() {
-        return this.nb_mort;
+        return this.#nb_mort;
     }
 }
--- js/class/user.js
+++ js/class/user.js
@@ -1,26 +1,32 @@
-class User{
+class User {
+
+
+    #point;
+    #degat;
+    #degatDPS;
+    #pnt;
 
     constructor() {
-        this.point = 0;
-        this.degat = 1;
-        this.degatDPS = 1;
-        this.pnt = 1;
+        this.#point = 0;
+        this.#degat = 1;
+        this.#degatDPS = 1;
+        this.#pnt = 1;
     }
 
     setPoint(pnt) {
-        this.point = this.point + pnt;
-        return this.point;
+        this.#point = this.#point + pnt;
+        return this.#point;
     }
 
     getDegat() {
-        return this.degat;
+        return this.#degat;
     }
 
     getDegatDPS() {
-        return this.degatDPS;
+        return this.#degatDPS;
     }
 
     getPnt() {
-        return this.pnt;
+        return this.#pnt;
     }
 }
